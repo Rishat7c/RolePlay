705d698f082403a55cc92f4b92664c7a31cd8dc4
Rishat <xpan96@gmail.com>
2018-10-10 22:59:04 +0300

    initial MVC

---
+++ MVC/MVC/View/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  MVC
+//
+//  Created by Ришат Гайсин on 10/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
