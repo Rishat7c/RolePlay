6b48f76d13fdfa21986a3a54bec8db960812abff
Rishat <xpan96@gmail.com>
2018-10-14 18:56:34 +0300

    switchChange method

--- UISwitch/UISwitch/ViewController.swift
+++ UISwitch/UISwitch/ViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let mySwitch = UISwitch() // Переключатель On/Off
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Создали свитч
+        self.mySwitch.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
+        self.view.addSubview(self.mySwitch)
+        
+        // On
+        self.mySwitch.setOn(true, animated: true)
+        
+        if self.mySwitch.isOn {
+            print("Свитч включен")
+        } else {
+            print("Свитч выключен")
+        }
+        
+        self.mySwitch.addTarget(self, action: #selector(switchChange(param:)), for: .valueChanged)
+        
+    }
+    
+    @objc func switchChange(param: UISwitch) {
+//        print("param is ", param)
+        
+        if param.isOn {
+            print("Свитч включен")
+        } else {
+            print("Свитч выключен")
+        }
     }
 
 
