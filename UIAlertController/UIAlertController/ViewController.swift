9fb201a3970fc17043e6c4c05cfe8a692010e030
Rishat <xpan96@gmail.com>
2018-10-14 17:27:03 +0300

    actions

--- UIAlertController/UIAlertController/ViewController.swift
+++ UIAlertController/UIAlertController/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func firstButton(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Error", message: "Hello, world!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            let text = alertController.textFields?.first?.text
+            print(text ?? "nil text")
+        }
+        
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(action)
+        self.present(alertController,animated: true,completion: nil)
+        
+    }
+    
 }
 
