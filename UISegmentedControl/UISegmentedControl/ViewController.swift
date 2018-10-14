d7d4e13fa5a7c19f43dbad18f9b43032cb33c0ed
Rishat <xpan96@gmail.com>
2018-10-14 22:24:26 +0300

    add target and surf in segment controller

--- UISegmentedControl/UISegmentedControl/ViewController.swift
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -12,17 +12,34 @@ class ViewController: UIViewController {
 
     var segmentControl = UISegmentedControl()
     var menuArray = ["one","two","thee"]
+    var imageView = UIImageView()
+    let imageArray = [UIImage(named: "c.png"),UIImage(named: "swift.png"),UIImage(named: "java.png")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.imageView.center = self.view.center
+        self.imageView.image = self.imageArray[0]
+        self.view.addSubview(imageView)
         
         // Создание сегмента
         self.segmentControl = UISegmentedControl(items: self.menuArray)
         self.segmentControl.frame = CGRect(x: 100, y: 500, width: 200, height: 30)
         self.view.addSubview(segmentControl)
         
+        segmentControl.addTarget(self, action: #selector(checkedControl), for: .valueChanged)
+        
     }
 
+    @objc func checkedControl(param: UISegmentedControl) {
+        if param == self.segmentControl {
+            let segmentIndex = param.selectedSegmentIndex
+            self.imageView.image = self.imageArray[segmentIndex]
+            let tfs = param.titleForSegment(at: segmentIndex)
+            print(tfs ?? "")
+        }
+    }
 
 }
 
