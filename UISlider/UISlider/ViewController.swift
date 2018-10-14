7d61add12c403af162969e0c18c54d87d0428c96
Rishat <xpan96@gmail.com>
2018-10-14 19:46:13 +0300

    audio volume and control audio volume

--- UISlider/UISlider/ViewController.swift
+++ UISlider/UISlider/ViewController.swift
@@ -12,13 +12,28 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var player = AVAudioPlayer()
+    let slider = UISlider()
+    
+    // outlet
+    @IBOutlet weak var sliderBoard: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Слайдер
+        self.slider.frame = CGRect(x: 0, y: 0, width: 200, height: 23)
+        self.slider.center = self.view.center
+        self.slider.minimumValue = 0.0
+        self.slider.maximumValue = 100.0
+        self.view.addSubview(slider)
+    
+        self.slider.addTarget(self, action: #selector(controlAudio(param:)), for: .valueChanged)
+        
         do {
             if let audioPath = Bundle.main.path(forResource: "audio", ofType: "mp3") {
                 try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+                self.slider.maximumValue = Float(player.duration)
             }
         } catch {
             print("Error")
@@ -26,6 +41,13 @@ class ViewController: UIViewController {
         
         self.player.play()
         
+    }
+    
+    //MARK: - Method
+    @objc func controlAudio(param: UISlider) {
+        if param == slider {
+            self.player.currentTime = TimeInterval(param.value)
+        }
     }
     
     //MARK: - Action
@@ -36,6 +58,11 @@ class ViewController: UIViewController {
     //MARK: - Action
     @IBAction func pauseAudio(_ sender: Any) {
         self.player.pause()
+    }
+    
+    
+    @IBAction func sliderAction(_ sender: Any) {
+        self.player.volume = self.sliderBoard.value
     }
     
 }
