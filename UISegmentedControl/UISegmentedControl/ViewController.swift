96ce01cc657131ae4e063b90206795089888c291
Rishat <xpan96@gmail.com>
2018-10-14 22:02:30 +0300

    base mvp

---
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by Ришат Гайсин on 14/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
