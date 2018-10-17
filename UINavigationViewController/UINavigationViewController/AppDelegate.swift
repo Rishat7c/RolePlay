04aaed3c01f2507da782c0c7465bfcc3cf0cc6c2
Rishat <xpan96@gmail.com>
2018-10-17 21:28:48 +0300

    initialize

--- UINavigationViewController/UINavigationViewController/AppDelegate.swift
+++ UINavigationViewController/UINavigationViewController/AppDelegate.swift
@@ -12,10 +12,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navController = UINavigationController()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let viewController = ViewController()
+        self.navController = UINavigationController(rootViewController: viewController)
+        
+        
+    
         return true
     }
 
