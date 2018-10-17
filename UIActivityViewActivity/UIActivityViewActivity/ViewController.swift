ce806260fde578e051f9d46307eabfc518c2a3e5
Rishat <xpan96@gmail.com>
2018-10-17 20:03:22 +0300

    base mvp

--- UIActivityViewActivity/UIActivityViewActivity/ViewController.swift
+++ UIActivityViewActivity/UIActivityViewActivity/ViewController.swift
@@ -23,8 +23,34 @@ class ViewController: UIViewController {
     func createTextField() {
         self.textField.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
         self.textField.center = self.view.center
+//        self.textField.borderStyle = UITextBorderStyle.roundedRect
+        self.textField.placeholder = "Enter text to share"
+        self.view.addSubview(self.textField)
+    }
+    
+    func createButton() {
+        self.buttonShare = UIButton(type: .roundedRect)
+        self.buttonShare.frame = CGRect(x: 50, y: 350, width: 280, height: 44)
+        self.buttonShare.setTitle("Sharred", for: .normal)
+        self.buttonShare.addTarget(self, action: #selector(submitButton), for: .touchUpInside)
+        self.view.addSubview(self.buttonShare)
         
     }
 
+    @objc func submitButton(param: Any) {
+        let text = self.textField.text
+        
+        if text?.characters.count == 0 {
+            let message = "С начало введите текст"
+            let alertController = UIAlertController(title: "Message", message: message, preferredStyle: .actionSheet)
+            let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(alertAction)
+            self.present(alertController,animated: true,completion: nil)
+        }
+        
+        self.activityViewController = UIActivityViewController(activityItems: <#T##[Any]#>, applicationActivities: <#T##[UIActivity]?#>)
+        
+    }
+    
 }
 
