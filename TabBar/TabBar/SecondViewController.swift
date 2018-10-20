8859aabb1703cf1b632a219425f46257ecc440b3
Rishat <xpan96@gmail.com>
2018-10-20 14:18:31 +0300

    created tabbar in code (delegate)

---
+++ TabBar/TabBar/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  TabBar
+//
+//  Created by Ришат Гайсин on 20/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
