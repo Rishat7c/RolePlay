5810e5c4c0030ae5e3a9ecae806bbdc5145b3646
Rishat <xpan96@gmail.com>
2018-10-20 14:08:47 +0300

    base

---
+++ TabBar/TabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBar
+//
+//  Created by Ришат Гайсин on 20/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
