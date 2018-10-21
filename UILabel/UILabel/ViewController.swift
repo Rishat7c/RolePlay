ddafaac6f98503d9e275c9da27354d3fbd778f53
Rishat <xpan96@gmail.com>
2018-10-21 19:48:39 +0300

    base

---
+++ UILabel/UILabel/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by Ришат Гайсин on 21/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var myLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
