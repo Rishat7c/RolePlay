e43f8d8c1363ce1a5dceeb4da7e6df3cab454239
Rishat <xpan96@gmail.com>
2018-10-21 20:05:38 +0300

    base

---
+++ UITextField/UITextField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextField
+//
+//  Created by Ришат Гайсин on 21/10/2018.
+//  Copyright © 2018 Ришат Гайсин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
