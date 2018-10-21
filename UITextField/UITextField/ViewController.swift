a8a763627aeb67e1f01e5b53754920968b31182d
Rishat <xpan96@gmail.com>
2018-10-21 21:32:38 +0300

    notification

--- UITextField/UITextField/ViewController.swift
+++ UITextField/UITextField/ViewController.swift
@@ -19,12 +19,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         self.textField.delegate = self // Подписываемся на текущий контроллер
         
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (param) in
+            self.view.frame.origin.y = -200.0 // Если textField не видно то отодвигаем его вверх и вытаскиваем клаву
+        }
+        
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { (param) in
+            self.view.frame.origin.x = 0.0 // Если textField не видно то отодвигаем его вверх и вытаскиваем клаву
+        }
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector(textFieldTextDidChange), name: UITextField.textDidChangeNotification, object: nil)
+        
     }
     
     //MARK: - CreateUI
     func createTextField() {
         let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 31)
-        
+
         self.textField = UITextField(frame: textFieldFrame)
         self.textField.borderStyle = UITextField.BorderStyle.roundedRect
         self.textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
@@ -34,44 +44,51 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.view.addSubview(self.textField)
     }
     
-    //MARK: - UITextFieldDelegate
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldBeginEditing = это я решаю можно ли редачить поле?")
-        return true
-    }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("textFieldDidBeginEditing = внимание! началось редактирование")
-    }
-   
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldEndEditing = закончилось редактирование")
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-        print("textFieldDidEndEditing")
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print("shouldChangeCharactersIn = вы ввели какую-то букву/символ = \(string)")
-        return true
-    }
-    
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        print("textFieldShouldClear = ты что-то очистил")
-        return true
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("textFieldShouldReturn = хотите убрать клаивиатуру?")
-        
-        if textField == self.textField {
-            self.textField.resignFirstResponder()
-        }
-        
-        return true
-    }
+//    MARK: - Notification
+//    @objc func textFieldTextDidChange(param: NSNotification) {
+//        print("textFieldTextDidChange = \(param)")
+//    }
+//
+//
+//    //MARK: - UITextFieldDelegate
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//        print("textFieldShouldBeginEditing = это я решаю можно ли редачить поле?")
+//        return true
+//    }
+//
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        print("textFieldDidBeginEditing = внимание! началось редактирование")
+//    }
+//
+//    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//        print("textFieldShouldEndEditing = закончилось редактирование")
+//        return true
+//    }
+//
+//    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+//        print("textFieldDidEndEditing")
+//    }
+//
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        print("shouldChangeCharactersIn = вы ввели какую-то букву/символ = \(string)")
+//        return true
+//    }
+//
+//    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+//        print("textFieldShouldClear = ты что-то очистил")
+//        return true
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        print("textFieldShouldReturn = хотите убрать клаивиатуру?")
+//
+//        if textField == self.textField {
+//            self.textField.resignFirstResponder()
+//        }
+//
+//        return true
+//    }
 
 }
 
