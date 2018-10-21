326af0e47b2055c29bf12ba6189a56fa8d5769a2
Rishat <xpan96@gmail.com>
2018-10-21 20:46:42 +0300

    textfield in frame - cgrect

--- UITextField/UITextField/ViewController.swift
+++ UITextField/UITextField/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var textField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 31)
+        
+        self.textField = UITextField(frame: textFieldFrame)
+        self.textField.borderStyle = UITextField.BorderStyle.roundedRect
+        self.textField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
+        self.textField.textAlignment = .center
+        self.textField.placeholder = "Hello, user"
+        self.textField.center = self.view.center
+        self.view.addSubview(self.textField)
+        
     }
 
 
