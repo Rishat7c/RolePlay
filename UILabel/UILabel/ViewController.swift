d0f2dea67371abd80cbfcabdddba21dcea0d8db3
Rishat <xpan96@gmail.com>
2018-10-21 20:02:40 +0300

    label funcs

--- UILabel/UILabel/ViewController.swift
+++ UILabel/UILabel/ViewController.swift
@@ -15,7 +15,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let labelFrame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        self.myLabel.frame = labelFrame
+        self.myLabel.text = "Привет, Ришат"
+        self.myLabel.numberOfLines = 0 // Кол-во линий если не вмещается предложение в лейбле
+        self.myLabel.lineBreakMode = .byWordWrapping // Обрыв по словам
+        self.myLabel.adjustsFontSizeToFitWidth = true // ...
+        self.myLabel.sizeToFit() // Ничего не хайдит
+        self.myLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        self.myLabel.center = self.view.center
+        self.myLabel.textColor = UIColor.black
+        self.myLabel.shadowColor = UIColor.lightGray
+        self.myLabel.shadowOffset = CGSize(width: 2, height: 2)
+        self.view.addSubview(self.myLabel)
         
     }
 
