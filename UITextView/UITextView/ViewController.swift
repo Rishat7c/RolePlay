983a73f08f017447d5f6dab108ff4eb398c415c8
Rishat <xpan96@gmail.com>
2018-10-28 21:31:35 +0300

    in view text

--- UITextView/UITextView/ViewController.swift
+++ UITextView/UITextView/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var myTextView = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myTextView = UITextView(frame: self.view.bounds)
+        myTextView.text = "Text here..."
+        myTextView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        myTextView.font = UIFont.systemFont(ofSize: 17)
+        self.view.addSubview(myTextView)
+        
     }
 
 
